a9046fe09b3a7319282e27568e0d58a9a4d11970
Bailey Benedict <bailey.bene23@gmail.com>
2022-10-31 14:59:03 -0500

    added footer bar

--- aggie-guide/src/App.js
+++ aggie-guide/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import Footer from './components/Footer';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,19 +8,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Footer />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Here we go Bailey Josh <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
